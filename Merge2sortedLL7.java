c9298d1ab695dfb41d35df4584c87c6d66d9eb4e
Abhayay42 <abhayth1441@gmail.com>
2022-12-12 13:29:03 +0530

    second commit

--- Merge2sortedLL7.java
+++ Merge2sortedLL7.java
@@ -31,12 +31,24 @@ public class Merge2sortedLL7 {
         Node<Integer> t1 = head1 , t2 = head2;
         Node<Integer> head = null , tail = null;
 
-        while(head1.next!=null && head2.next!=null){
-            
+        if(t1.data<=t2.data){
+            head = t1 ;
+            tail = t1 ;
+            t1 = t1.next;
+        }
+        if(t2.data<=t1.data){
+            head = t2;
+            tail = t2;
+            t2 = t2.next;
         }
 
+        while(t1.next!=null && t2.next!=null){
+            if(t1.data<=t2.data){
+                
+            }
+        }
 
-    }
+        }
 
     public static void main(String[] args) {
         Node<Integer> head1 = input();
